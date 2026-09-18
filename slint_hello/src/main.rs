0ee0131c33341f73362c98fffb94eeedf5f22f24
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-09-19 07:17:20 +0900

    build: added slint_hello

---
+++ slint_hello/src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    ui.run()?;
+
+    Ok(())
+}
